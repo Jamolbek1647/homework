61caefee43138e6bfb3d8ccbd417122e967525da
Jamolbek <jamolbekikromov@gmail.com>
2023-02-20 11:52:00 +0500

    add pagination

--- main.js
+++ main.js
@@ -1,130 +1,137 @@
+class PageOptions {
+  constructor(page, size){
+    this.page = page;
+    this.size = size;
+  }
+}
 class Person {
-    constructor(firstname, lastname, age, address) {
-        this.firstname = firstname
-        this.lastname = lastname
-        this.age = age
-        this.address = address
-    }
+  constructor(firstname, lastname, age, address) {
+    this.firstname = firstname
+    this.lastname = lastname
+    this.age = age
+    this.address = address
+  }
 }
 
 class Address {
-    constructor(region, district, street, apartment) {
-        this.region = region
-        this.district = district
-        this.street = street
-        this.apartment = apartment
-    }
+  constructor(region, district, street, apartment) {
+    this.region = region
+    this.district = district
+    this.street = street
+    this.apartment = apartment
+  }
 
 
-    //DOM
-    // console.log(document);
-    // document.getElementById('myDiv');
-    // const myDiv = document.getElementById('myDiv')
-    // console.log(myDiv);
+  //DOM
+  // console.log(document);
+  // document.getElementById('myDiv');
+  // const myDiv = document.getElementById('myDiv')
+  // console.log(myDiv);
 
-    // myDiv.textContent = 'test text content' //faqat text yozish uchun
+  // myDiv.textContent = 'test text content' //faqat text yozish uchun
 
-    // myDiv.innerHTML = '<strong>test text content</strong>' // htmldagi amallarni bajarsa bo'ladi yani html tiliga o'giradi
+  // myDiv.innerHTML = '<strong>test text content</strong>' // htmldagi amallarni bajarsa bo'ladi yani html tiliga o'giradi
 
-    // const myH1 = document.createElement('h1')
-    // myH1.style = 'font-weight: 600'
-    // const mySpam = document.createElement('span')
-    // mySpam.innerHTML = 'This is spam';
-    // mySpam.style = 'color: blue'
-    // myH1.appendChild(mySpam)
-    // myDiv.innerHTML = myH1.innerHTML 
-    // console.log(myH1);
+  // const myH1 = document.createElement('h1')
+  // myH1.style = 'font-weight: 600'
+  // const mySpam = document.createElement('span')
+  // mySpam.innerHTML = 'This is spam';
+  // mySpam.style = 'color: blue'
+  // myH1.appendChild(mySpam)
+  // myDiv.innerHTML = myH1.innerHTML 
+  // console.log(myH1);
 
 
-    //query selector
-    // const myQuery = document.querySelectorAll('.li-item')
-    // console.log(myQuery.length);
-    // for (const iterator of myQuery) {
-    //     console.log(iterator.innerHTML);
+  //query selector
+  // const myQuery = document.querySelectorAll('.li-item')
+  // console.log(myQuery.length);
+  // for (const iterator of myQuery) {
+  //     console.log(iterator.innerHTML);
 }
+const pageOptions = new PageOptions(4,10)
 
 let persons = []
 // Massivni tekshirishga
 const PersonList = JSON.parse(localStorage.getItem('persons'))
-    ? JSON.parse(localStorage.getItem('persons'))
-    : []
+  ? JSON.parse(localStorage.getItem('persons'))
+  : []
 
 //Button bosishda mal olsin
 const myBtn = document.getElementById('myBtn')
 myBtn.addEventListener('click', (e) => {
-    // console.log(e);
-    // const firstname = document.getElementById('firstname');
-    // const lastname = document.getElementById('lastname');
-    // console.log(firstname.value, lastname.value);
-    
-    const myForm = document.getElementById('myForm')
-    // const myForm = document.forms.myForm
-    let _firstname = myForm.elements.firstname.value
-    let _lastname = myForm.elements.lastname.value
-    let _age = myForm.elements.age.value
-    let _region = myForm.elements.region.value
-    let _district = myForm.elements.district.value
-    let _street = myForm.elements.street.value
-    let _apartment = myForm.elements.apartment.value
+  // console.log(e);
+  // const firstname = document.getElementById('firstname');
+  // const lastname = document.getElementById('lastname');
+  // console.log(firstname.value, lastname.value);
 
-    // persons.push(new Person(
-    //   _firstname, _lastname, _age, new Address(_region, _district, _street, _apartment)))
+  const myForm = document.getElementById('myForm')
+  // const myForm = document.forms.myForm
+  let _firstname = myForm.elements.firstname.value
+  let _lastname = myForm.elements.lastname.value
+  let _age = myForm.elements.age.value
+  let _region = myForm.elements.region.value
+  let _district = myForm.elements.district.value
+  let _street = myForm.elements.street.value
+  let _apartment = myForm.elements.apartment.value
 
-
-    persons.push({
-      id: persons.length + 1,
-      firstname: _firstname,
-      lastname: _lastname,
-      age: _age,
-      region: _region,
-      district: _district,
-      street: _street,
-      apartment: _apartment
-    
-    }
-    )
-    // localdagi malumotlarni qayta yuklab olyapti
-    localStorage.setItem("persons", JSON.stringify(persons));
-    // function print(){
-    //     let response = document.getElementById('print')
-    // return response.value = JSON.stringify(persons[0])
-    // } 
-    // console.log(print());
+  // persons.push(new Person(
+  //   _firstname, _lastname, _age, new Address(_region, _district, _street, _apartment)))
 
 
-    PersonList.push(persons)
-    console.log(PersonList);
-    // localStorage.setItem('persons', JSON.stringify(persons_list))
-    // const get_from_local = localStorage.getItem('persons')
-    // console.log(get_from_local);
-    
-    // console.log(persons);
-    // console.log(myForm.elements.age.value);
+  persons.push({
+    id: persons.length + 1,
+    firstname: _firstname,
+    lastname: _lastname,
+    age: _age,
+    region: _region,
+    district: _district,
+    street: _street,
+    apartment: _apartment
+
+  }
+  )
+  // localdagi malumotlarni qayta yuklab olyapti
+  localStorage.setItem("persons", JSON.stringify(persons));
+  // function print(){
+  //     let response = document.getElementById('print')
+  // return response.value = JSON.stringify(persons[0])
+  // } 
+  // console.log(print());
+
+
+  PersonList.push(persons)
+  console.log(PersonList);
+  // localStorage.setItem('persons', JSON.stringify(persons_list))
+  // const get_from_local = localStorage.getItem('persons')
+  // console.log(get_from_local);
+
+  // console.log(persons);
+  // console.log(myForm.elements.age.value);
 })
 
 const selectViloyat = document.getElementById("selectViloyat")
 
 const viloyats = [
-    {id: 1, text: "Toshkent"},
-    {id: 2, text: "Sirdaryo"},
-    {id: 3, text: "Jizzax"},
-    {id: 4, text: "Samarqand"},
-    {id: 5, text: "Navoiy"},
-    {id: 6, text: "Buxoro"},
-    {id: 7, text: "Qashqadaryo"},
-    {id: 8, text: "Surxondaryo"},
-    {id: 9, text: "Xorazm"},
-    {id: 10, text: "Andijon"},
-    {id: 11, text: "Namangan"},
-    {id: 12, text: "Farg'ona"},
+  { id: 1, text: "Toshkent" },
+  { id: 2, text: "Sirdaryo" },
+  { id: 3, text: "Jizzax" },
+  { id: 4, text: "Samarqand" },
+  { id: 5, text: "Navoiy" },
+  { id: 6, text: "Buxoro" },
+  { id: 7, text: "Qashqadaryo" },
+  { id: 8, text: "Surxondaryo" },
+  { id: 9, text: "Xorazm" },
+  { id: 10, text: "Andijon" },
+  { id: 11, text: "Namangan" },
+  { id: 12, text: "Farg'ona" },
 ]
 viloyats.forEach((viloyat) => {
-    const opt = document.createElement("option");
-    
+  const opt = document.createElement("option");
 
-    opt.text = viloyat.text;
-    // console.log(opt);
-    selectViloyat.appendChild(opt);
+
+  opt.text = viloyat.text;
+  // console.log(opt);
+  selectViloyat.appendChild(opt);
 });
 
 const persons_from_locale = localStorage.getItem("persons");
@@ -147,10 +154,10 @@ function loadTable() {
   table_body.innerHTML = "";
   //  e-obyekt k- indeks 
   persons.map((e, k) => {
-    if (page == Math.floor(k/10) + 1) {
+    if (page == Math.floor(k / 10) + 1) {
       table_body.innerHTML += addRow(e);
     }
-  });
+  }); 
 }
 loadTable();
 function addRow(item) {
@@ -176,3 +183,38 @@ function addRow(item) {
         </button></td>
 </tr>`;
 }
+
+// function addNew() {
+//   clear();
+//   openModal();
+// }
+
+// function close() {
+//   // const modal = document.getElementById('exampleModal');
+//   var myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+//   myModal.hide();
+
+// }
+
+// function openModal() {
+
+//   var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+//   myModal.show();
+// }
+
+function drawPagination() {
+  const pagination = document.querySelector('.pagination');
+  const previous = `<li class="page-item">
+  <a class="page-link" href="#" aria-label="Previous">
+    <span aria-hidden="true">&laquo;</span>
+  </a>
+</li>`
+  const numbers = ``;
+  const Pagecount = Math.floor((persons.length/pageOptions.page));
+  const next = `<li class="page-item">
+  <a class="page-link" href="#" aria-label="Next">
+    <span aria-hidden="true">&raquo;</span>
+  </a>
+</li>`
+}
+drawPagination();
