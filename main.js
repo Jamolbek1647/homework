1e88ee89a2d9f4418ea38df53e2db207f6f4743f
Jamolbek1647 <jamolbekikromov@mail.com>
2023-02-24 01:16:57 +0500

    pagination completed

--- main.js
+++ main.js
@@ -10,41 +10,56 @@ class PageOptions {
 // const pageOptions = new PageOptions(1,10)
 class Person {
   constructor(firstname, lastname, age, address) {
-    this.firstname = firstname
-    this.lastname = lastname
-    this.age = age
-    this.address = address
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.age = age;
+    this.address = address;
   }
 }
 
 class Address {
   constructor(region, district, street, apartment) {
-    this.region = region
-    this.district = district
-    this.street = street
-    this.apartment = apartment
+    this.region = region;
+    this.district = district;
+    this.street = street;
+    this.apartment = apartment;
   }
 }
-const pageOptions = new PageOptions(1, 10)
 
+const pageOptions = new PageOptions(1, 10);
+//Save to local storage
+function saveToLocale() {
+  localStorage.setItem("persons", JSON.stringify(persons));
+}
+
+// Local Storagedan ma'lumotlarni tekshirib ejranga chiqarish
+//Get from local storage
+function syncFromLocale() {
+  const personsString = localStorage.getItem("persons");
+  if (personsString) {
+    const _persons = JSON.parse(personsString);
+    if (Array.isArray(_persons)) {
+      persons = [..._persons];
+    }
+  }
+}
 
 // Massivni tekshirishga
-const PersonList = JSON.parse(localStorage.getItem('persons'))
-  ? JSON.parse(localStorage.getItem('persons'))
-  : []
+const PersonList = JSON.parse(localStorage.getItem("persons"))
+  ? JSON.parse(localStorage.getItem("persons"))
+  : [];
 
 //Button bosishda mal olsin
-const myBtn = document.getElementById('myBtn')
-myBtn.addEventListener('click', (e) => {
+const myBtn = document.getElementById("myBtn");
+myBtn.addEventListener("click", (e) => {
   save();
   // console.log(persons);
   loadTable();
-}
-)
+});
 // localdagi malumotlarni qayta yuklab olyapti
-localStorage.setItem("persons", JSON.stringify(persons));
+// localStorage.setItem("persons", JSON.stringify(persons));
 
-PersonList.push(persons)
+PersonList.push(persons);
 // console.log(PersonList);
 
 // })
@@ -65,7 +80,7 @@ function save() {
 
   if (personId && personId > 0) {
     //edit
-    const _item = persons.find(a => a.id == personId);
+    const _item = persons.find((a) => a.id == personId);
     _item.firstname = _firstname;
     _item.age = _age;
     _item.lastname = _lastname;
@@ -73,9 +88,7 @@ function save() {
     _item.district = _district;
     _item.street = _street;
     _item.apartment = _apartment;
-
-  }
-  else {
+  } else {
     //add
     persons.push({
       id: persons.length + 1,
@@ -85,15 +98,13 @@ function save() {
       region: _region,
       district: _district,
       street: _street,
-      apartment: _apartment
-
-    }
-    )
+      apartment: _apartment,
+    });
     clear();
     saveToLocale();
   }
 }
-const selectViloyat = document.getElementById("selectViloyat")
+const selectViloyat = document.getElementById("selectViloyat");
 
 const viloyats = [
   { id: 1, text: "Toshkent" },
@@ -108,7 +119,7 @@ const viloyats = [
   { id: 10, text: "Andijon" },
   { id: 11, text: "Namangan" },
   { id: 12, text: "Farg'ona" },
-]
+];
 viloyats.forEach((viloyat) => {
   const opt = document.createElement("option");
 
@@ -118,15 +129,14 @@ viloyats.forEach((viloyat) => {
 });
 
 function editItem(id) {
-
   //persons find item
-  const item = persons.find(a => a.id === id);
+  const item = persons.find((a) => a.id === id);
   console.log(item);
   if (item) {
-    console.log('item' + item)
-    //form elementlariga valuelarni set qilish 
+    console.log("item" + item);
+    //form elementlariga valuelarni set qilish
     const myForm = document.forms.myForm;
-    myForm.elements.personId.value = id;//identificator
+    myForm.elements.personId.value = id; //identificator
     myForm.elements.age.value = item.age;
     myForm.elements.firstname.value = item.firstname;
     myForm.elements.lastname.value = item.lastname;
@@ -135,25 +145,26 @@ function editItem(id) {
     myForm.elements.apartment.value = item.apartment;
     openModal();
   }
-
 }
 
-const table_body = document.getElementById('mytable');
+const table_body = document.getElementById("mytable");
 
 function loadTable() {
-  table_body.innerHTML = '';
+  table_body.innerHTML = "";
   //  sahifalash
   const start = (pageOptions.page - 1) * pageOptions.pageSize;
-  const pagePersons = persons.slice(start, start + pageOptions.pageSize)
+  const pagePersons = persons.slice(start, start + pageOptions.pageSize);
   pagePersons.forEach((e, i) => {
     table_body.innerHTML += addRow(e, i + 1);
-  })
+  });
 }
 loadTable();
 
 function addRow(item, index) {
   return ` <tr id='tr_${index}' onclick="selectRow(${index})">
-    <td>${index*(pageOptions.pageSize)}</td>
+    <td>${
+      pageOptions.page * pageOptions.pageSize - (pageOptions.pageSize - index)
+    }</td>
     <td>${item.lastname}</td>
     <td>${item.firstname}</td>
     <td>${item.age}</td>
@@ -174,65 +185,45 @@ function addRow(item, index) {
 </tr>`;
 }
 function selectRow(e) {
-  console.log(e)
+    console.log(e);
+    const tr = document.getElementById('tr_' + e);
+    tr.classList.add("person-row-selected");
 }
-
 
 loadTable();
 function addNew() {
-  clear();
-  openModal();
-
+    clear();
+    openModal();
 }
 
 function close() {
   // const modal = document.getElementById('exampleModal');
-  var myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+  var myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
   myModal.hide();
-
 }
 
 function clear() {
   const myForm = document.forms.myForm;
   myForm.reset();
-
 }
 
 function openModal() {
-  var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+  var myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
   myModal.show();
 }
 
 function deleteItem(id) {
-  const item = persons.find(a => a.id === id);
+  const item = persons.find((a) => a.id === id);
   const index = persons.indexOf(item);
   persons.splice(index, 1);
   saveToLocale();
   loadTable();
 }
 
-//Save to local storage
-function saveToLocale() {
-  localStorage.setItem('persons', JSON.stringify(persons));
-}
-
-// Local Storagedan ma'lumotlarni tekshirib ejranga chiqarish
-//Get from local storage
-function syncFromLocale() {
-  const personsString = localStorage.getItem('persons');
-  if (personsString) {
-    const _persons = JSON.parse(personsString);
-    if (Array.isArray(_persons)) {
-      persons = [..._persons]
-    }
-  }
-}
-
-
 //PAGINATION
 loadTable();
 function drawPagination() {
-  const pagination = document.querySelector('.pagination');
+  const pagination = document.querySelector(".pagination");
   const next = `
 <li class="page-item" >
     <a class="page-link" href="javascript:(0);" aria-label="Next" onclick="next()">
@@ -240,7 +231,7 @@ function drawPagination() {
     </a>
 </li>`;
   let numbers = ``;
-  const pageCount = Math.ceil((persons.length / pageOptions.pageSize));
+  const pageCount = Math.ceil(persons.length / pageOptions.pageSize);
   const prev = `<li class="page-item">
 <a class="page-link" href="javascript:(0);" aria-label="Previous"  onclick="prev()">
     <span aria-hidden="true">&laquo;</span>
@@ -250,19 +241,21 @@ function drawPagination() {
   console.log(pageOptions.page);
   for (let index = 0; index < pageCount; index++) {
     const pageNumber = index + 1;
-    numbers += `<li class="page-item ${pageNumber == pageOptions.page ? 'active' : ''}"><a class="page-link" href="javascript:(0);" onclick="onPageChanged(${pageNumber})">${pageNumber}</a></li>`;
+    numbers += `<li class="page-item ${
+      pageNumber == pageOptions.page ? "active" : ""
+    }"><a class="page-link" href="javascript:(0);" onclick="onPageChanged(${pageNumber})">${pageNumber}</a></li>`;
   }
   pagination.innerHTML = prev + numbers + next;
 }
 drawPagination();
 function onPageChanged(number) {
-  console.log(number)
+  console.log(number);
   pageOptions.page = number;
   loadTable();
   drawPagination();
 }
 function prev() {
-  console.log('prev', persons.length)
+  console.log("prev", persons.length);
   if (pageOptions.page > 1) {
     pageOptions.page -= 1;
     loadTable();
@@ -270,8 +263,8 @@ function prev() {
   }
 }
 function next() {
-  const pageCount = Math.ceil((persons.length / pageOptions.pageSize));
-  console.log('next', persons.length)
+  const pageCount = Math.ceil(persons.length / pageOptions.pageSize);
+  console.log("next", persons.length);
   if (pageOptions.page < pageCount) {
     pageOptions.page += 1;
     loadTable();
